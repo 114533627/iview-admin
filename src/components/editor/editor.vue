281df5c98968c6661daf70e01d31c0f3243a0dfb
xh <114533627@qq.com>
2021-07-16 14:54:16 +0800

    wicco 第5次提交 1

--- src/components/editor/editor.vue
+++ src/components/editor/editor.vue
@@ -44,7 +44,7 @@ export default {
        */
     cache: {
       type: Boolean,
-      default: true
+      default: false
     },
     uploadParam: {
       type: Object
@@ -52,7 +52,7 @@ export default {
   },
   watch: {
     value () {
-      this.setHtml(this.value)
+      this.setHtml(this.value ? this.value : '')
     }
   },
   computed: {
@@ -116,8 +116,8 @@ export default {
     // create这个方法一定要在所有配置项之后调用
     this.editor.create()
     // 如果本地有存储加载本地存储内容
-    let html = this.value || localStorage.editorCache
-    if (html) this.editor.txt.html(html)
+    let html = this.value || (this.cache) ? localStorage.editorCache : ''
+    this.editor.txt.html(html)
   },
   beforeDestroy () {
     // this.editor._offAllEvent()
